9ba68e589465efba0f6d5a87a63952e7fe811e18
Valentin Maier <valentin16.maier@gmail.com>
2024-06-05 19:55:52 +0200

    Implemented TimerOne library for periodic PID update calls

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <Arduino.h>
+#include <TimerOne.h>
 #include "EEPROMHandler.hpp"
 #include "fastprotoc.hpp"
 
@@ -33,8 +34,8 @@ const int eeKpAddress = sizeof(float);                      // EEPROM address of
 const int eeKiAddress = 2 * sizeof(float);                  // EEPROM address of stored integral gain
 const int eeKdAddress = 3 * sizeof(float);                  // EEPROM address of stored derivative gain
 const byte LED_PINS[] = {2, 3, 4, 5, 6, 7, 11, 12, 9, 8};   // Pins connected to LEDs
-const int SETPOINT_LOW = 140;                               // Lower limit of the setpoint
-const int SETPOINT_HIGH = 210;                              // Upper limit of the setpoint
+const int SETPOINT_LOW = 100;                               // Lower limit of the setpoint
+const int SETPOINT_HIGH = 300;                              // Upper limit of the setpoint
 const int INTEGRAL_SATURATION = 2000;                       // Integral saturation value
 const float DT = 0.1;                                       // Time interval for derivative calculation
 const unsigned long PACKET_SEND_INTERVAL = 20;              // Interval for sending packets (in milliseconds)
@@ -51,12 +52,13 @@ float prevKp = 0.0;                                         // Previous proporti
 float prevKi = 0.0;                                         // Previous integral gain
 float prevKd = 0.0;                                         // Previous derivative gain
 
-int hallValue = 0;                                          // Current Hall sensor reading
+volatile int hallValue = 0;                                 // Current Hall sensor reading
 float output = 0.0;                                         // PID output value
 float integral = 0.0;                                       // Integral term of PID controller
 float derivative = 0.0;                                     // Derivative term of PID controller
 float error = 0.0;                                          // Error between setpoint and measured value
 float previousError = 0.0;                                  // Previous error for derivative calculation
+volatile int electromagnetSetting = 0;                      // PWM signal variable
 float triangularSignalStep = 20.0;                          // Step value for triangular signal
 unsigned long triangularSignalTime = 0;                     // Time for triangular signal update
 unsigned long previousSignalTime = 0;                       // Time for previous signal update
@@ -64,10 +66,13 @@ unsigned long previousEEPROMTime = 0;                       // Time for previous
 
 EEPROMHandler eepromHandler;
 
-void handleSerialInput();
 void updatePID();
+void handleSerialInput();
+void sendData();
 
 void setup() {
+  Timer1.initialize(500);
+  Timer1.attachInterrupt(updatePID);
   Serial.begin(BAUD_RATE);
 
   pinMode(COIL_OUTPUT_PIN, OUTPUT);
@@ -91,7 +96,7 @@ void setup() {
 
 void loop() {
   handleSerialInput();
-  updatePID();
+  sendData();
 }
 
 /**
@@ -109,7 +114,7 @@ void handleSerialInput() {
 
     switch (packet.identifier) {
       case SETPOINT_UPDATE:
-        setpoint = data;
+        setpoint = constrain(data, SETPOINT_LOW, SETPOINT_HIGH);
         break;
       case KP_UPDATE:
         Kp = data;
@@ -130,10 +135,12 @@ void handleSerialInput() {
     if (currentTime - previousEEPROMTime < EEPROM_WRITE_INTERVAL) return;
     previousEEPROMTime = currentTime;
 
+    noInterrupts();
     if (setpoint != prevSetpoint) eepromHandler.set(eeSetpointAddress, setpoint);
     if (Kp != prevKp) eepromHandler.set(eeKpAddress, Kp);
     if (Ki != prevKi) eepromHandler.set(eeKiAddress, Ki);
     if (Kd != prevKd) eepromHandler.set(eeKdAddress, Kd);
+    interrupts();
   }
 }
 
@@ -146,17 +153,15 @@ void handleSerialInput() {
  * periodically over serial communication.
  */
 void updatePID() {
+#ifdef TRIANGLE_ENABLE
   unsigned long currentTime = millis();
 
-#ifdef TRIANGLE_ENABLE
   if (currentTime - triangularSignalTime >= PACKET_SEND_INTERVAL) {
     triangularSignalTime = currentTime;
     setpoint += triangularSignalStep;
 
     if (setpoint >= SETPOINT_HIGH || setpoint <= SETPOINT_LOW)
       triangularSignalStep *= -1;
-
-    sendPacket(SETPOINT_UPDATE, setpoint);
   }
 #endif
 
@@ -165,18 +170,37 @@ void updatePID() {
   error = (setpoint - hallValue);
   integral = constrain(integral + error * DT, -INTEGRAL_SATURATION, INTEGRAL_SATURATION);
   derivative = (error - previousError) / DT;
+  previousError = error;
 
   output = 0 - Kp * error - Ki * integral - Kd * derivative;
-  int electromagnetSetting = constrain(output, 0, 255);
+  electromagnetSetting = constrain(output, 0, 255);
   if (hallValue > HALL_VALUE_THRESHOLD)
     electromagnetSetting = 0;
 
   analogWrite(COIL_OUTPUT_PIN, electromagnetSetting);
+}
 
-  currentTime = millis();
+/**
+ * @brief Sends data to a client.
+ * 
+ * This function periodically sends update packets over serial communication.
+ * It sends Hall sensor readings and electromagnet PWM settings to the client
+ * at a predefined interval. Additionally, it sends updates for the setpoint,
+ * proportional, integral, and derivative gains whenever they change.
+ */
+void sendData() {
+  int hallValueCopy;
+  int electromagnetSettingCopy;
+  unsigned long currentTime = millis();
+
+  noInterrupts();
+  hallValueCopy = hallValue;
+  electromagnetSettingCopy = electromagnetSetting;
+  interrupts();
+
   if (currentTime - previousSignalTime >= PACKET_SEND_INTERVAL) {
-    sendPacket(HALL_UPDATE, hallValue);
-    sendPacket(PWM_UPDATE, electromagnetSetting);
+    sendPacket(HALL_UPDATE, hallValueCopy);
+    sendPacket(PWM_UPDATE, electromagnetSettingCopy);
     previousSignalTime = currentTime;
   }
 
@@ -185,7 +209,6 @@ void updatePID() {
   if (Ki != prevKi) sendPacket(KI_UPDATE, Ki);
   if (Kd != prevKd) sendPacket(KD_UPDATE, Kd);
 
-  previousError = error;
   prevSetpoint = setpoint;
   prevKp = Kp;
   prevKi = Ki;
